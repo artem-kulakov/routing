0fb4448e89c8728f54ecd2ea24b7f2a9b87853d7
Artem Kulakov <artem.kulakov@gmail.com>
2024-07-30 20:02:55 +0300

    Ring handler

---
+++ src/routing/main.clj
@@ -0,0 +1,12 @@
+(ns routing.main
+  (:require [ring.adapter.jetty :as jetty]))
+
+(defn handler
+  [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
+
+(defn -main
+  [& args]
+  (jetty/run-jetty handler {:port 3000}))

c8f1cc31582e61450f22dd788de464ff55468416
Artem Kulakov <artem.kulakov@gmail.com>
2024-07-30 21:30:55 +0300

    s

--- src/routing/main.clj
+++ src/routing/main.clj
@@ -3,30 +3,31 @@
             [ring.middleware.reload :refer [wrap-reload]]
             [clojure.pprint :as pp]))
 
-;; (defn lists
-;;   []
-;;   {:status 200
-;;    :headers {"Content-Type" "text/html"}
-;;    :body "Lists page"})
+(def lists
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Lists page"})
 
-;; (defn info
-;;   []
-;;   {:status 200
-;;    :headers {"Content-Type" "text/html"}
-;;    :body "Info page"})
+(def info
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Info page"})
+
+(def not-found
+  {:status 404})
 
 (defn handler
   [request]
   ;; (pp/pprint request)
   (let [route {(:request-method request) (:uri request)}]
     (case route
-      {:get "/lists"} {:status 200
-                       :headers {"Content-Type" "text/html"}
-                       :body "Lists page"}
-      {:get "/info"} {:status 200
-                      :headers {"Content-Type" "text/html"}
-                      :body "Info page"})))
+      {:get "/lists"} lists
+      {:get "/info"} info
+      not-found)))
+
+(def app
+  (wrap-reload handler))
 
 (defn -main
   [& args]
-  (jetty/run-jetty (wrap-reload handler) {:port 3000}))
+  (jetty/run-jetty app {:port 3000}))

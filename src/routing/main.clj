a69ad4fad14873a36b533dcdf79e33a8dd3d7f29
Artem Kulakov <artem.kulakov@gmail.com>
2024-07-30 21:22:55 +0300

    s

--- src/routing/main.clj
+++ src/routing/main.clj
@@ -3,14 +3,29 @@
             [ring.middleware.reload :refer [wrap-reload]]
             [clojure.pprint :as pp]))
 
+;; (defn lists
+;;   []
+;;   {:status 200
+;;    :headers {"Content-Type" "text/html"}
+;;    :body "Lists page"})
+
+;; (defn info
+;;   []
+;;   {:status 200
+;;    :headers {"Content-Type" "text/html"}
+;;    :body "Info page"})
+
 (defn handler
   [request]
   ;; (pp/pprint request)
   (let [route {(:request-method request) (:uri request)}]
-    (println route))
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
+    (case route
+      {:get "/lists"} {:status 200
+                       :headers {"Content-Type" "text/html"}
+                       :body "Lists page"}
+      {:get "/info"} {:status 200
+                      :headers {"Content-Type" "text/html"}
+                      :body "Info page"})))
 
 (defn -main
   [& args]
